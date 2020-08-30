29341790e0df40ad2c0ea6895faa64d1893c428b
Ryan Altimari <ryanaltimari8@gmail.com>
2020-08-30 12:11:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,57 @@
-# Write your code here.
+require 'pry'
+
+
+  def line(array)
+    try = []
+    names = ["The line is currently:"]
+    answer = ["The line is currently:"]
+  if
+    array.length == 0
+    puts "The line is currently empty."
+  else
+    i = 0
+    while i < array.length 
+    names << array[i]
+    i += 1
+  end
+  names.join("', '")
+  names
+  binding.pry
+  
+  
+  
+  end
+  end
+
+
+
+  
+  
+
+
+
+
+
+
+
+
+
+def take_a_number(array, person)
+  array.push(person)
+  puts "Welcome, #{person}. You are number #{array.length} in line."
+  return array
+end
+
+
+def now_serving(array)
+  
+  if array.length < 1 
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Currently serving #{array[0]}."
+    array.shift
+    return array
+  end
+    
+end
+  
